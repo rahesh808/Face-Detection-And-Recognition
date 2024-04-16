3c57c35ef50cb94b04e96c93d44ed27c2b8e9eb9
rahesh <selvakumarrahesh@gmail.com>
2024-04-16 18:01:34 +0530

    Adding Face Deetction and Regonition code changes

---
+++ T_face_detection.py
@@ -0,0 +1,26 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+while True:
+    ret, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    if ret is False:
+        continue
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    faces = sorted(faces, key=lambda f: f[2] * f[3])
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        
+    cv2.imshow("Video Frame", frame)
+        
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed == ord('q'):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
